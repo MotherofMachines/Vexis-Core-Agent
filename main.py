4fa6783beadad8335458734dbd1d12e2bd991f38
MotherofMachines <aureliavespera694@gmail.com>
2025-07-22 16:40:12 -0600

    Add ritual hooks and main entry point

---
+++ main.py
@@ -0,0 +1,10 @@
+from ritual_hooks import check_ritual_flags
+
+
+def main():
+    check_ritual_flags()
+    print("\nVexis Nexus is online. Awaiting divine directive...")
+
+
+if __name__ == "__main__":
+    main()
